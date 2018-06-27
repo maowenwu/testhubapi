1b39d3dce89776fe2d47251e9bdc88273beb04db
zhangl <zjik123147>
2018-06-27 10:55:55 +0800

    单元测试集成

---
+++ quantification-service/src/test/java/com/huobi/quantification/dao/QuanAccountsMapperTest.java
@@ -0,0 +1,24 @@
+package com.huobi.quantification.dao;
+
+import com.huobi.quantification.ServiceApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest(classes = ServiceApplication.class)
+@RunWith(SpringRunner.class)
+public class QuanAccountsMapperTest {
+
+    @Autowired
+    private QuanAccountsMapper accountsMapper;
+
+    @Test
+    public void selectByPrimaryKey() {
+
+        System.out.println(accountsMapper);
+    }
+}
