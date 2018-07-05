76a90b7f9b8ab117951c0fc7cfc45d4398ee313e
zhangl <zjik123147>
2018-07-05 10:52:39 +0800

    开发

--- quantification-biz/src/main/java/com/huobi/quantification/service/job/impl/JobServiceImpl.java
+++ quantification-biz/src/main/java/com/huobi/quantification/service/job/impl/JobServiceImpl.java
@@ -2,7 +2,8 @@ package com.huobi.quantification.service.job.impl;
 
 import com.huobi.quantification.dao.QuanJobFutureMapper;
 import com.huobi.quantification.entity.QuanJobFuture;
-import com.huobi.quantification.job.okcoin.OkFutureDepthJob;
+import com.huobi.quantification.enums.ExchangeEnum;
+import com.huobi.quantification.job.okcoin.*;
 import com.huobi.quantification.quartz.QuartzManager;
 import com.huobi.quantification.service.job.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,10 +25,44 @@ public class JobServiceImpl implements JobService {
         List<QuanJobFuture> jobFutures = quanJobFutureMapper.selectAll();
         for (QuanJobFuture jobFuture : jobFutures) {
             if (jobFuture.getState().equals(1)) {
-                quartzManager.addJobNoRepeat(jobFuture.getJobName(), OkFutureDepthJob.class, jobFuture.getCron(), jobFuture);
+                Class jobClass = getJobClass(jobFuture.getExchangeId(), jobFuture.getJobType());
+                if (jobClass == null) {
+                    continue;
+                }
+                quartzManager.addJobNoRepeat(jobFuture.getJobName(), jobClass, jobFuture.getCron(), jobFuture);
             } else {
                 quartzManager.removeJobNoRepeat(jobFuture.getJobName());
             }
         }
     }
+
+    private Class getJobClass(int exchangeId, int jobType) {
+        Class clazz = null;
+        if (exchangeId == ExchangeEnum.OKEX.getExId()) {
+            switch (jobType) {
+                case 1:
+                    clazz = OkFutureDepthJob.class;
+                    break;
+                case 2:
+                    clazz = OkFutureKlineJob.class;
+                    break;
+                case 3:
+                    clazz = OkFutureTickerJob.class;
+                    break;
+                case 4:
+                    clazz = OkFutureUserInfoJob.class;
+                    break;
+                case 5:
+                    clazz = OkFuturePositionJob.class;
+                    break;
+                case 6:
+                    clazz = OkFutureOrderJob.class;
+                    break;
+                default:
+                    clazz = null;
+            }
+        }
+        return clazz;
+    }
+
 }
