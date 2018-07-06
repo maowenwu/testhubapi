32bc14e0d9b1ef4faa6c6096d409895837d29c95
lichenyang <lichenyang@DJX7SN2.HuoBiINC.com>
2018-07-06 18:19:29 +0800

    开发

--- quantification-biz/src/main/java/com/huobi/quantification/service/order/impl/OrderHuobiServiceImpl.java
+++ quantification-biz/src/main/java/com/huobi/quantification/service/order/impl/OrderHuobiServiceImpl.java
@@ -1,5 +1,6 @@
 package com.huobi.quantification.service.order.impl;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,6 +13,7 @@ import com.huobi.quantification.dao.QuanOrderMapper;
 import com.huobi.quantification.entity.QuanOrder;
 import com.huobi.quantification.service.http.HttpService;
 import com.huobi.quantification.service.order.OrderHuobiService;
+
 /**
  * @author shaoxiaofeng
  * @since 2018/6/26
@@ -21,83 +23,100 @@ public class OrderHuobiServiceImpl implements OrderHuobiService {
 	private HttpService httpService;
 	@Autowired
 	private QuanOrderMapper quanOrderMapper;
+
 	/**
-     * 获取订单信息
-     *
-     * @return
-     */
-    public Object getHuobiOrderInfo() {
-    	 Map<String, String> params = new HashMap<>();
-         String body = httpService.doPost(HttpConstant.HUOBI_ORDERDETAIL, params);
-         parseAndSaveOrderInfo(body);
-         return null;
-    }
+	 * 获取订单信息
+	 *
+	 * @return
+	 */
+	public Object getHuobiOrderInfo() {
+		Map<String, String> params = new HashMap<>();
+		String body = httpService.doPost(HttpConstant.HUOBI_ORDERDETAIL, params);
+		parseAndSaveOrderInfo(body);
+		return null;
+	}
 
-    /**
-     * 批量获取订单信息
-     *
-     * @return
-     */
-    public Object getHuobiOrdersInfo(){
-    	return null;
-    }
+	/**
+	 * 批量获取订单信息
+	 *
+	 * @return
+	 */
+	public Object getHuobiOrdersInfo() {
+		return null;
+	}
 
-    /**
-     * 查询成交（时间顺序）
-     *
-     * @return
-     */
-    public Object getHuobiOrdersHistory(){
-    	return null;
-    }
-    /**
-     * 下单
-     *
-     * @return
-     */
-    public Object placeHuobiOrder(){
-    	 Map<String, String> params = new HashMap<>();
-         params.put("account-id", "");
-         params.put("amount", "");
-         params.put("price", "");
-         params.put("source", "");
-         params.put("symbol", "ethusdt");
-         params.put("type", "buy-limit");
-         String result = httpService.doPost(HttpConstant.HUOBI_ORDER_PLACE, params);
-         return null;
-    }
+	/**
+	 * 查询成交（时间顺序）
+	 *
+	 * @return
+	 */
+	public Object getHuobiOrdersHistory() {
+		return null;
+	}
 
-    /**
-     * 批量下单
-     *
-     * @return
-     */
-    public Object placeHuobikOrders(){
-    	return null;
-    }
-    /**
-     * 撤单
-     *
-     * @return
-     */
-    public Object cancelHuobiOrder(){
-    	return null;
-    }
-    /**
-     * 批量撤单
-     *
-     * @return
-     */
-    public Object cancelHuobiOrders(){
-    	return null;
-    }
-    private void parseAndSaveOrderInfo(String body) {
-    	 JSONObject jsonObject = JSON.parseObject(body);
-    		JSONObject jsonObjectdata=jsonObject.getJSONObject("data");
-    		QuanOrder quanOrder=new QuanOrder();
-    		quanOrder.setOrderState(jsonObjectdata.getString("state"));
-    		//quanOrder.setOrderAmount(Long.valueOf(jsonObjectdata.getString("amount")));
-    		quanOrder.setOrderSymbol(jsonObjectdata.getString("symbol"));
-    		quanOrderMapper.insert(quanOrder);
-    }
+	/**
+	 * 下单
+	 *
+	 * @return
+	 */
+	public Object placeHuobiOrder() {
+		Map<String, String> params = new HashMap<>();
+		params.put("account-id", "");
+		params.put("amount", "");
+		params.put("price", "");
+		params.put("source", "");
+		params.put("symbol", "ethusdt");
+		params.put("type", "buy-limit");
+		String result = httpService.doPost(HttpConstant.HUOBI_ORDER_PLACE, params);
+		return null;
+	}
+
+	/**
+	 * 批量下单
+	 *
+	 * @return
+	 */
+	public Object placeHuobikOrders() {
+		return null;
+	}
+
+	/**
+	 * 撤单
+	 *
+	 * @return
+	 */
+	public Object cancelHuobiOrder() {
+		return null;
+	}
+
+	/**
+	 * 批量撤单
+	 *
+	 * @return
+	 */
+	public Object cancelHuobiOrders() {
+		return null;
+	}
+
+	private void parseAndSaveOrderInfo(String body) {
+		JSONObject jsonObject = JSON.parseObject(body);
+		JSONObject jsonObjectdata = jsonObject.getJSONObject("data");
+		QuanOrder quanOrder = new QuanOrder();
+		quanOrder.setOrderSourceId(jsonObjectdata.getLong("id"));
+		quanOrder.setOrderSymbol(jsonObjectdata.getString("symbol"));
+		quanOrder.setOrderAccountId(jsonObjectdata.getLong("account-id"));
+		quanOrder.setOrderAmount(new BigDecimal(jsonObjectdata.getString("amount")));
+		quanOrder.setOrderPrice(new BigDecimal(jsonObjectdata.getString("price")));
+		quanOrder.setOrderCreatedAt(jsonObjectdata.getDate("created-at"));
+		quanOrder.setOrderType(jsonObjectdata.getString("type"));
+		quanOrder.setOrderFieldAmount(new BigDecimal(jsonObjectdata.getString("field-amount")));
+		quanOrder.setOrderFieldCashAmount(new BigDecimal(jsonObjectdata.getString("field-cash-amount")));
+		quanOrder.setOrderFieldFees(new BigDecimal(jsonObjectdata.getString("field-fees")));
+		quanOrder.setOrderFinishedAt(jsonObjectdata.getDate("finished-at"));
+		quanOrder.setOrderAccountId(jsonObjectdata.getLong("user-id"));
+		quanOrder.setOrderSource(jsonObjectdata.getString("source"));
+		quanOrder.setOrderState(jsonObjectdata.getString("state"));
+		quanOrder.setOrderCanceledAt(jsonObjectdata.getDate("canceled-at"));
+		quanOrderMapper.insert(quanOrder);
+	}
 }
--- quantification-service/src/test/java/com/huobi/quantification/service/order/impl/OrderHuobiServiceImplTest.java
+++ quantification-service/src/test/java/com/huobi/quantification/service/order/impl/OrderHuobiServiceImplTest.java
@@ -1,13 +1,68 @@
 package com.huobi.quantification.service.order.impl;
 
+import java.math.BigDecimal;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.huobi.quantification.ServiceApplication;
+import com.huobi.quantification.dao.QuanOrderMapper;
+import com.huobi.quantification.entity.QuanOrder;
 
 @SpringBootTest(classes = ServiceApplication.class)
 @RunWith(SpringRunner.class)
 public class OrderHuobiServiceImplTest {
-
+	
+	@Autowired
+	private QuanOrderMapper quanOrderMapper;
+	
+	@Test
+	public void getOrder() {
+		String string = "{\r\n" + 
+				"  \"status\": \"ok\",\r\n" + 
+				"  \"data\": {\r\n" + 
+				"    \"id\": 59378,\r\n" + 
+				"    \"symbol\": \"ethusdt\",\r\n" + 
+				"    \"account-id\": 100009,\r\n" + 
+				"    \"amount\": \"10.1000000000\",\r\n" + 
+				"    \"price\": \"100.1000000000\",\r\n" + 
+				"    \"created-at\": 1494901162595,\r\n" + 
+				"    \"type\": \"buy-limit\",\r\n" + 
+				"    \"field-amount\": \"10.1000000000\",\r\n" + 
+				"    \"field-cash-amount\": \"1011.0100000000\",\r\n" + 
+				"    \"field-fees\": \"0.0202000000\",\r\n" + 
+				"    \"finished-at\": 1494901400468,\r\n" + 
+				"    \"user-id\": 1000,\r\n" + 
+				"    \"source\": \"api\",\r\n" + 
+				"    \"state\": \"filled\",\r\n" + 
+				"    \"canceled-at\": 0,\r\n" + 
+				"    \"exchange\": \"huobi\",\r\n" + 
+				"    \"batch\": \"\"\r\n" + 
+				"  }\r\n" + 
+				"}";
+		JSONObject jsonObject = JSON.parseObject(string);
+		JSONObject jsonObjectdata = jsonObject.getJSONObject("data");
+		QuanOrder quanOrder = new QuanOrder();
+		quanOrder.setOrderSourceId(jsonObjectdata.getLong("id"));
+		quanOrder.setOrderSymbol(jsonObjectdata.getString("symbol"));
+		quanOrder.setOrderAccountId(jsonObjectdata.getLong("account-id"));
+		quanOrder.setOrderAmount(new BigDecimal(jsonObjectdata.getString("amount")));
+		quanOrder.setOrderPrice(new BigDecimal(jsonObjectdata.getString("price")));
+		quanOrder.setOrderCreatedAt(jsonObjectdata.getDate("created-at"));
+		quanOrder.setOrderType(jsonObjectdata.getString("type"));
+		quanOrder.setOrderFieldAmount(new BigDecimal(jsonObjectdata.getString("field-amount")));
+		quanOrder.setOrderFieldCashAmount(new BigDecimal(jsonObjectdata.getString("field-cash-amount")));
+		quanOrder.setOrderFieldFees(new BigDecimal(jsonObjectdata.getString("field-fees")));
+		quanOrder.setOrderFinishedAt(jsonObjectdata.getDate("finished-at"));
+		quanOrder.setOrderAccountId(jsonObjectdata.getLong("user-id"));
+		quanOrder.setOrderSource(jsonObjectdata.getString("source"));
+		quanOrder.setOrderState(jsonObjectdata.getString("state"));
+		quanOrder.setOrderCanceledAt(jsonObjectdata.getDate("canceled-at"));
+		quanOrderMapper.insert(quanOrder);
+	}
 }
