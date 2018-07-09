89f96f2b4000fdf9dcef0a0facae1f978fb8d072
HUOBIINC\shaoxiaofeng <shaoxiaofeng@huobi.com>
2018-07-09 19:52:10 +0800

    初始化

--- quantification-biz/src/main/java/com/huobi/quantification/huobi/response/BalanceBean.java
+++ quantification-biz/src/main/java/com/huobi/quantification/huobi/response/BalanceBean.java
@@ -1,9 +1,8 @@
 package com.huobi.quantification.huobi.response;
 
 /**
- * @Author ISME
- * @Date 2018/1/14
- * @Time 16:23
+ * @author shaoxiaofeng
+ * @since 2018/6/29
  */
 
 public class BalanceBean {
