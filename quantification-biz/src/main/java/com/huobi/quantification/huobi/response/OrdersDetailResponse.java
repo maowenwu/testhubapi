8b45415ada62573b76d1ce803bcd98b71d4165c7
HUOBIINC\shaoxiaofeng <shaoxiaofeng@huobi.com>
2018-07-09 19:48:58 +0800

    添加注释

--- quantification-biz/src/main/java/com/huobi/quantification/huobi/response/OrdersDetailResponse.java
+++ quantification-biz/src/main/java/com/huobi/quantification/huobi/response/OrdersDetailResponse.java
@@ -1,9 +1,8 @@
 package com.huobi.quantification.huobi.response;
 
 /**
- * @Author ISME
- * @Date 2018/1/14
- * @Time 18:21
+ * @author shaoxiaofeng
+ * @since 2018/6/29
  */
 
 public class OrdersDetailResponse<T> {
--- quantification-biz/src/main/java/com/huobi/quantification/huobi/response/Place.java
+++ quantification-biz/src/main/java/com/huobi/quantification/huobi/response/Place.java
@@ -1,9 +1,8 @@
 package com.huobi.quantification.huobi.response;
 
 /**
- * @Author ISME
- * @Date 2018/1/14
- * @Time 17:10
+ * @author shaoxiaofeng
+ * @since 2018/6/29
  */
 
 public class Place {
