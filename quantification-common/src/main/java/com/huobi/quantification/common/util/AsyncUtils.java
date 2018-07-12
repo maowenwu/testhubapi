da51d040d54cd71a670d7fb6613fc806cc20d64b
zhangl <zjik123147>
2018-07-12 09:52:28 +0800

    开发

--- quantification-common/src/main/java/com/huobi/quantification/common/util/AsyncUtils.java
+++ quantification-common/src/main/java/com/huobi/quantification/common/util/AsyncUtils.java
@@ -3,17 +3,15 @@ package com.huobi.quantification.common.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
 
 public class AsyncUtils {
 
     private static Logger logger = LoggerFactory.getLogger(AsyncUtils.class);
 
-    private static ExecutorService executor = Executors.newFixedThreadPool(200,new ThreadFactory() {
+    private static ExecutorService executor = Executors.newFixedThreadPool(200, new ThreadFactory() {
         private final AtomicInteger nextId = new AtomicInteger();
 
         @Override
@@ -32,7 +30,33 @@ public class AsyncUtils {
             } catch (Throwable e) {
                 logger.error("异步任务异常：", e);
             }
-        },executor);
+        }, executor);
         return future;
+    }
+
+    /**
+     * 方法超时后会设置中断标志，使用该方法需要自行处理中断
+     *
+     * @param supplier
+     * @param milliseconds 超时时间 单位毫秒
+     * @param <U>
+     * @return
+     * @throws ExecutionException
+     * @throws TimeoutException
+     */
+    public static <U> U supplyAsync(Supplier<U> supplier, long milliseconds) throws ExecutionException, TimeoutException {
+        Future<U> future = executor.submit(() -> supplier.get());
+        U u = null;
+        try {
+            u = future.get(milliseconds, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            logger.error("异步任务异常：", e);
+        } catch (ExecutionException e) {
+            throw e;
+        } catch (TimeoutException e) {
+            future.cancel(true);
+            throw e;
+        }
+        return u;
     }
 }
