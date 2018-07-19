d27d56d1a04c24c51a1862232f8c929dcd2a50db
lichenyang <lichenyang@DJX7SN2.HuoBiINC.com>
2018-07-19 12:28:01 +0800

    开发

---
+++ quantification-biz/src/main/java/com/huobi/quantification/job/huobi/spot/HuobiCurrentPriceJob.java
@@ -0,0 +1,19 @@
+package com.huobi.quantification.job.huobi.spot;
+
+import org.quartz.DisallowConcurrentExecution;
+
+import com.huobi.quantification.common.context.ApplicationContextHolder;
+import com.huobi.quantification.dto.JobParamDto;
+import com.huobi.quantification.job.AbstractQuartzJob;
+import com.huobi.quantification.service.market.HuobiMarketService;
+
+@DisallowConcurrentExecution
+public class HuobiCurrentPriceJob extends AbstractQuartzJob{
+
+	@Override
+	public void execute(JobParamDto data) {
+		HuobiMarketService marketHuobiService = ApplicationContextHolder.getContext().getBean(HuobiMarketService.class);
+		marketHuobiService.updateCurrentPrice(data.getSymbol());
+	}
+
+}
