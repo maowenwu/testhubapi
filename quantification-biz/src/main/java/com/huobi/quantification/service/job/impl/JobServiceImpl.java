a3b875de51cda01eebc3b84d37599128846a9d54
zhangl <zjik123147>
2018-07-20 16:49:42 +0800

    开发

--- quantification-biz/src/main/java/com/huobi/quantification/service/job/impl/JobServiceImpl.java
+++ quantification-biz/src/main/java/com/huobi/quantification/service/job/impl/JobServiceImpl.java
@@ -69,8 +69,15 @@ public class JobServiceImpl implements JobService {
         jobMap.put(ExchangeEnum.HUOBI.getExId(), huobiSpotTypeClass);
     }
 
+    private boolean flag = true;
+
     @Override
     public void updateFutureJobScheduler() {
+        if (flag) {
+            flag = false;
+        } else {
+            return;
+        }
         List<QuanJobFuture> jobFutures = quanJobFutureMapper.selectAll();
         for (QuanJobFuture jobFuture : jobFutures) {
             if (jobFuture.getState().equals(1)) {
