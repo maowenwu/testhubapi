ddad8d9821beb4ac1467208011b66b77a01697ac
HUOBIINC\maowenwu <838294470@qq.com>
2018-07-28 16:18:26 +0800

    修改数据类型

--- quantification-strategy/src/main/java/com/huobi/quantification/strategy/hedging/StartHedgingParam.java
+++ quantification-strategy/src/main/java/com/huobi/quantification/strategy/hedging/StartHedgingParam.java
@@ -6,80 +6,69 @@ public class StartHedgingParam {
 
 	private String baseCoin;
 	private String quoteCoin;
-	private Long spotAccountID;
-	private int spotExchangeId;
-	private Long furureAccountID;
-	private Long furureExchangeId;
+	private Long spotAccountID;//处理现货账户id
+	private Integer spotExchangeId;//现货交易所id
+	private Long futureAccountID;//处理期货账户id
+	private Integer futureExchangeId;//期货交易所id
 	private BigDecimal slippage;//滑点
 	private BigDecimal feeRate;//手续费率
-
-
+	private String contractCode;//合约code
 	public String getBaseCoin() {
 		return baseCoin;
 	}
-
 	public void setBaseCoin(String baseCoin) {
 		this.baseCoin = baseCoin;
 	}
-
 	public String getQuoteCoin() {
 		return quoteCoin;
 	}
-
 	public void setQuoteCoin(String quoteCoin) {
 		this.quoteCoin = quoteCoin;
 	}
-
 	public Long getSpotAccountID() {
 		return spotAccountID;
 	}
-
 	public void setSpotAccountID(Long spotAccountID) {
 		this.spotAccountID = spotAccountID;
 	}
-
-	public Long getFurureAccountID() {
-		return furureAccountID;
-	}
-
-	public void setFurureAccountIDB(Long furureAccountID) {
-		this.furureAccountID = furureAccountID;
-	}
-
 	public int getSpotExchangeId() {
 		return spotExchangeId;
 	}
-
 	public void setSpotExchangeId(int spotExchangeId) {
 		this.spotExchangeId = spotExchangeId;
 	}
-
-	public Long getFurureExchangeId() {
-		return furureExchangeId;
+	public Long getFutureAccountID() {
+		return futureAccountID;
 	}
-
-	public void setFurureExchangeId(Long furureExchangeId) {
-		this.furureExchangeId = furureExchangeId;
+	public void setFutureAccountID(Long futureAccountID) {
+		this.futureAccountID = futureAccountID;
 	}
-
+	public Integer getFutureExchangeId() {
+		return futureExchangeId;
+	}
+	public void setFutureExchangeId(Integer futureExchangeId) {
+		this.futureExchangeId = futureExchangeId;
+	}
 	public BigDecimal getSlippage() {
 		return slippage;
 	}
-
 	public void setSlippage(BigDecimal slippage) {
 		this.slippage = slippage;
 	}
-
-	public void setFurureAccountID(Long furureAccountID) {
-		this.furureAccountID = furureAccountID;
-	}
-
 	public BigDecimal getFeeRate() {
 		return feeRate;
 	}
-
 	public void setFeeRate(BigDecimal feeRate) {
 		this.feeRate = feeRate;
 	}
+	public String getContractCode() {
+		return contractCode;
+	}
+	public void setContractCode(String contractCode) {
+		this.contractCode = contractCode;
+	}
 
+	
+
+	
 }
